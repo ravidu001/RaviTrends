f5f6dbf5776d2981e7780514b9b324c9757a4d4e
ravidu001 <ravidupeiris2002@gmail.com>
2025-05-23 11:11:07 +0530

    feat: add routing and create initial page components for the application

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ npm install
 npm install react-router-dom react-toastify
 npm run dev
 install tailwind and postcss.config
+create components, pages and context(logics) in the src folder
+create About, Cart, Collection, Contact, Home, Login, Orders, PlaceOrder, Product in the pages folder to create the routes
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
+import { Route, Routes } from 'react-router-dom'
+
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
 import React from 'react'
 
 const App = () => {
   return (
-    <div>My App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/Contact' element={<Contact/>} />
+        <Route path='/product/:prodcutId' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/place-order' element={<PlaceOrder/>} />
+        <Route path='/orders' element={<Orders/>} />
+      </Routes>
+    </div>
   )
 }
 

866cc0aa4cd986b0dc1cdbff9305dae455ec8648
ravidu001 <ravidupeiris2002@gmail.com>
2025-05-25 00:47:51 +0530

    feat: wrap logo in Link component for navigation and update active link styles in index.css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
     const [visible, setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-        <img src={assets.logo8} alt="logo" className='w-36' />       
+        <Link to='/' className='flex items-center gap-2'>
+            <img src={assets.logo8} alt="logo" className='w-36' />       
+        </Link>
 
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700 hidden'> 
             <NavLink to='/' className='flex flex-col items-center gap-1'>

0d5fe9ce6bd84f7c27d1e33f917a1dfd14cfb618
ravidu001 <ravidupeiris2002@gmail.com>
2025-05-25 10:59:15 +0530

    feat: add OurPolicy component to display policy information on Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import BestSeller from '../components/BestSeller'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import OurPolicy from '../components/OurPolicy'
 import React from 'react'
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
         <Hero />
         <LatestCollection />
         <BestSeller />
+        <OurPolicy />
     </div>
   )
 }

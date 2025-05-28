159fcfca4e8dbe393a2fdf96a9854e740decb422
ravidu001 <ravidupeiris2002@gmail.com>
2025-05-28 21:03:40 +0530

    feat: enhance navigation in Cart and PlaceOrder components by integrating useNavigate for seamless routing

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -12,6 +13,7 @@ const ShopContextProvider = (props) => {
     const [search,setSearch] = useState('');    
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
         
@@ -96,7 +98,7 @@ const ShopContextProvider = (props) => {
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCartCount, updateQunatity,
-        getCartAmount
+        getCartAmount, navigate,
     }
     
     return (

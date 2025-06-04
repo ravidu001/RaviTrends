cf82a907757401984e46f59ae5160b08a9458362
ravidu001 <ravidupeiris2002@gmail.com>
2025-06-04 12:00:19 +0530

    feat: update Navbar to link to login page and enhance Login component with form handling and state management

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,7 +37,10 @@ const Navbar = () => {
                 <img onClick={()=>setShowSearch(true)} src={assets.search_icon} alt="search" className='w-5 cursor-pointer' />
 
                 <div className='group relative'>
-                    <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="profile icon" />
+                    <Link to='/login'>
+                        <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="profile icon" />
+                    </Link>
+                    
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded-lg shadow-lg'> 
                             <p className='cursor-pointer hover:text-black'>My Profile</p>

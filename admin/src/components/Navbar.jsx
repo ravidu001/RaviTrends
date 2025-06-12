3f7fafe5b635a6b29a5cdaa4597ee5b187742e3e
ravidu001 <ravidupeiris2002@gmail.com>
2025-06-12 12:32:51 +0530

    feat: implement login  and logout functionality and update token management in App component

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
         <div className='flex gap-2'>
             <img className='w-[max(20%,10px)]' src={assets.logo8} alt="" />
             <img className='w-[max(5%,10px)]' src={assets.logo} alt="" />
         </div>
-        <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+        <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }

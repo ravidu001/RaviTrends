1383f7c46f56ff02f112a659c6ba0d8f8d874407
ravidu001 <ravidupeiris2002@gmail.com>
2025-06-19 20:15:13 +0530

    feat: enhance cart functionality with user authentication

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,16 +1,17 @@
 import { addToCart, getUserCart, updateCart } from '../controllers/cartController';
 
+import authUser from '../middleware/auth';
 import express from 'express';
 
 const cartRouter = express.Router();
 
 // Route to add a product to the user's cart
-cartRouter.post('/add', addToCart);
+cartRouter.post('/add', authUser, addToCart);
 
 // Route to update the user's cart
-cartRouter.post('/update', updateCart);
+cartRouter.post('/update', authUser, updateCart);
 
 // Route to get the user's cart data
-cartRouter.post('/get', getUserCart);
+cartRouter.post('/get', authUser, getUserCart);
 
 export default cartRouter;

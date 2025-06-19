42804b54b37375d10de32b8338fa4c9c2d9c25a5
ravidu001 <ravidupeiris2002@gmail.com>
2025-06-19 22:03:08 +0530

    feat: implement order routes for managing user and admin order functionalities

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,33 @@
+
+
+// Placing orders using COD Method
+const placeOrder = async (req, res) => {
+
+}
+
+// Placing orders using Stripe Method
+const placeOrderStripe = async (req, res) => {
+
+}
+
+// Placing orders using Razorpay Method
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+// All Order data for Admin Panel
+const allOrders = async (req, res) => {
+
+}
+
+// User Order Data For Frontend
+const userOrders = async (req, res) => {
+
+}
+
+// Update Order Status from Admin Panel
+const updateStatus = async (req, res) => {
+
+}
+
+export { placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, userOrders, updateStatus }
---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,21 @@
+import { allOrders, placeOrder, placeOrderRazorpay, placeOrderStripe, updateStatus, userOrders } from '../controllers/orderController.js'
+
+import adminAuth from '../middleware/adminAuth.js'
+import authUser from '../middleware/auth.js'
+import express from 'express'
+
+const orderRouter = express.Router();
+
+// Admin Features
+orderRouter.post('/list', adminAuth, allOrders);
+orderRouter.post('/status', adminAuth, updateStatus);
+
+// Payment Features
+orderRouter.post('/place', authUser, placeOrder);
+orderRouter.post('/stripe', authUser, placeOrderStripe);
+orderRouter.post('/razorpay', authUser, placeOrderRazorpay);
+
+// User Features
+orderRouter.post('/userorders', adminAuth, userOrders);
+
+export default orderRouter;
